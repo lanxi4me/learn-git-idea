1e82c72f041609e6aef93f50f1f28c321a1ce6b7
ilove <1499596712@qq.com>
2022-09-08 08:50:03 +0800

    good good good

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("add a new line");
-        System.out.println("remote things, ha ha ha，顶顶顶顶顶");
+        System.out.println("good goog good");
     }
 }
